4a9d97411cef260c30a8d5598ac5f6f3d702e48a
KevinHaeusler <kevin@kh.dev>
2024-07-16 19:15:51 +0200

    Update plex_request to use hata http client. Still need to figure out how to actually use the button in the same handler so I can return the selected media.

--- haruki/plugins/plex_request/overseer_search.py
+++ haruki/plugins/plex_request/overseer_search.py
@@ -1,55 +1,43 @@
-import overseerr
+from ...bots import Kiruha
 from ...constants import OVERSEER_TOKEN, OVERSEER_URL
 
-configuration = overseerr.Configuration(
-    host=f"{OVERSEER_URL}/api/v1",
-
-)
-
-# Configure API key authorization: apiKey
-configuration.api_key['apiKey'] = OVERSEER_TOKEN
+headers = {"x-api-key": OVERSEER_TOKEN, "content-type": "application/json"}
 
 
 async def get_search_results(media_type, media):
-    with overseerr.ApiClient(configuration) as api_client:
-        # Create an instance of the API class
-        api_instance = overseerr.SearchApi(api_client)
-        query = media  # str |
-        page = 1  # float |  (optional) (default to 1)
-        language = 'en'  # str |  (optional)
+    params = {
+        "page":     1,
+        "language": "en",
+        "query":    media,
+    }
+    async with Kiruha.http.get(f'{OVERSEER_URL}/api/v1/search', headers, params=params) as response:
+        api_response = await response.json()
+    search_results = []
+    for result in api_response.get('results', []):
+        if result.get('mediaType') == media_type:
+            if media_type == 'tv':
+                search_result = {
+                    "id":    result.get('id'),
+                    "title": result.get('name'),
+                    "year":  result.get('firstAirDate', '')[:4],
+                }
+            else:
+                search_result = {
+                    "id":    result.get('id'),
+                    "title": result.get('title'),
+                    "year":  result.get('releaseDate', '')[:4],
 
-        try:
-            # Search for movies, TV shows, or people
-            api_response = api_instance.get_search(query, page=page, language=language)
-            results = api_response.results if api_response.results else []
+                }
+            search_results.append(search_result)
+    print(search_results)
+    return search_results
 
-            # Access the 'results' attribute which is a list of results
-            search_results = []
-            for result in results:
-                actual_instance = result.actual_instance
-                if actual_instance.media_type == media_type:
-                    # if it's tv show
-                    if media_type == 'tv':
-                        search_result = {
-                            "id":    actual_instance.id,
-                            "title": actual_instance.name,
-                            "year":  actual_instance.first_air_date[:4] if actual_instance.first_air_date else None
-                        }
-                    else:  # if it's something else
-                        search_result = {
-                            "id":    actual_instance.id,
-                            "title": actual_instance.title,
-                            "year":  actual_instance.release_date[:4] if actual_instance.release_date else None
-                        }
-                    # retrieving plexUrl if it already exists
-                    plex_url = actual_instance.media_info.additional_properties.get('plexUrl')
-                    if plex_url:
-                        plex_url = plex_url + ' '
-                        search_result['plexUrl'] = plex_url
-                    # append result to the search results list
-                    search_results.append(search_result)
 
-            return search_results
-        except Exception as e:
-            print("Exception when calling SearchApi->get_search: %s\n" % e)
-            return 'Search Failed'
+async def get_media_info(media_type, media_id):
+    params = {
+        "language": "en",
+    }
+    async with Kiruha.http.get(f'{OVERSEER_URL}/api/v1/movie/{media_id}', headers, params=params) as response:
+        print(response)
+        api_response = await response.json()
+        return api_response
--- haruki/plugins/plex_request/plex_request.py
+++ haruki/plugins/plex_request/plex_request.py
@@ -1,28 +1,75 @@
 __all__ = ()
 
+from hata import Embed
+from hata.ext.slash import Select, Option, InteractionResponse, Button, ButtonStyle, Row
 from haruki.bots import Kiruha
-from haruki.plugins.plex_request.overseer_search import get_search_results
+from haruki.plugins.plex_request.overseer_search import get_search_results, get_media_info
 
-CUSTOM_ID_REQUEST = 'request'
-CUSTOM_ID_ABORT = 'abort'
+tmdb_image_url = 'https://image.tmdb.org/t/p/w300_and_h450_face'
+
 PLEX_REQUEST_ID = 'plex_request'
+PLEX_REQUEST_MEDIA = 'plex_request_media'
+PLEX_REQUEST_ABORT = 'plex_request_abort'
 
 MEDIA_TYPES = [
     'tv',
     'movie',
 ]
 
+BUTTON_REQUEST = Button('Request', custom_id=PLEX_REQUEST_MEDIA, style=ButtonStyle.green)
+BUTTON_ABORT = Button('Abort', custom_id=PLEX_REQUEST_ABORT, style=ButtonStyle.red)
+
 
 @Kiruha.interactions(is_global=True)
-async def plex_request(media_type: ('str', 'Pick Media Type'), media: ('str', 'Enter the media to search for')):
-    media_data = await get_search_results(media_type, media)
-    return media_data
+async def plex_request(media_type: (MEDIA_TYPES, 'Pick Media Type'), media: ('str', 'Enter the media to search for')):
+    media_list = await get_search_results(media_type, media)
+    embed = Embed(f'Searching for {media_type.title()}: {media.title()}')
+    options = []
+    for element in media_list:
+        name = element['title']
+        year = element['year']
+        id = element['id']
+        if name:
+            title = f"{name} ({year})"
+            options.append(Option(str(id), title))
+    # Create the Select
+    select = [
+        Row(
+            Select(
+                options,
+                custom_id=PLEX_REQUEST_ID, )
+        ),
+        Row(
+            Button('Request', custom_id=PLEX_REQUEST_MEDIA, style=ButtonStyle.green),
+            Button('Abort', custom_id=PLEX_REQUEST_ABORT, style=ButtonStyle.red))]
+
+    return InteractionResponse(embed=embed, components=select)
 
 
-@plex_request.autocomplete('media_type')
-async def autocomplete_media_type(value):
-    if value is None:
-        return MEDIA_TYPES[:25]
+@Kiruha.interactions(custom_id=[PLEX_REQUEST_ID, PLEX_REQUEST_MEDIA, PLEX_REQUEST_ABORT])
+async def handle_media_selection(event):
+    # We filter out 3rd party users based on original and current invoking user.
+    if event.message.interaction.user_id != event.user_id:
+        return
+    selected_media_list = event.values
+    # Second we filter out incorrect selected values.
+    if selected_media_list is None:
+        return
+    selected_media = selected_media_list[0]
+    print(event.interaction.custom_id)
+    if event.interaction.custom_id == "plex_request_abort":
+        print("RIP")
+    elif event.interaction.custom_id == "plex_request_media":
+        print("Yay")
 
-    value = value.casefold()
-    return [media_type for media_type in MEDIA_TYPES if (value in media_type)]
+    movie_info = await get_media_info("movie", selected_media)
+    url = tmdb_image_url + movie_info['posterPath']
+    embed = Embed(f'{movie_info["title"]} - {movie_info["releaseDate"].split("-")[0]}')
+    embed.add_image(url)
+    embed.description = (
+        f'{movie_info["overview"]} \n'
+
+    )
+    yield InteractionResponse(embed=embed)
+
+
