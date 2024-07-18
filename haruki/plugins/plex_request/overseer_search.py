4dccc0831bea709551bdc1f5d550cf512a709ed2
KevinHaeusler <kevin@kh.dev>
2024-07-18 16:43:02 +0200

    One Huge Commit where I refactored it and done some serious warcrimes by how jank it is.

--- haruki/plugins/plex_request/overseer_search.py
+++ haruki/plugins/plex_request/overseer_search.py
@@ -1,45 +1,103 @@
+__all__ = ()
+
+from json import dumps
+import urllib.parse
+
 from ...bots import Kiruha
 from ...constants import OVERSEER_TOKEN, OVERSEER_URL
 
-headers = {"x-api-key": OVERSEER_TOKEN, "content-type": "application/json"}
 
+class MediaSearch:
+    def __init__(self):
+        self.headers = {"x-api-key": OVERSEER_TOKEN, "content-type": "application/json"}
+        self.media_list = []
 
-# TODO refactor as class
+    async def get_search_results(self, media_type, media):
+        params = {
+            "page":     1,
+            "language": "en",
+            "query":    urllib.parse.quote(media),
+        }
+        async with Kiruha.http.get(f'{OVERSEER_URL}/api/v1/search', self.headers, params=params) as response:
+            api_response = await response.json()
+        self.media_list = []
+        print(api_response)
+        for result in api_response.get('results', []):
+            if result.get('mediaType') == media_type:
+                if media_type == 'tv':
+                    search_result = {
+                        "id":         result.get('id'),
+                        "title":      result.get('name')[:30],
+                        "year":       result.get('firstAirDate', '')[:4],
+                        "media_type": result.get('mediaType'),
+                    }
+                else:
+                    search_result = {
+                        "id":         result.get('id'),
+                        "title":      result.get('title')[:30],
+                        "year":       result.get('releaseDate', '')[:4],
+                        "media_type": result.get('mediaType'),
+                    }
+                self.media_list.append(search_result)
+        return self.media_list
 
-async def get_search_results(media_type, media):
-    params = {
-        "page":     1,
-        "language": "en",
-        "query":    media,
-    }
-    async with Kiruha.http.get(f'{OVERSEER_URL}/api/v1/search', headers, params=params) as response:
-        api_response = await response.json()
-    search_results = []
-    for result in api_response.get('results', []):
-        if result.get('mediaType') == media_type:
+    async def get_selected_media_info(self, selected_id):
+        for media in self.media_list:
+            if media['id'] == selected_id:
+                return await self.get_media_info(media['type'], selected_id)
+        return None
+
+    def get_media_list(self):
+        return self.media_list
+
+    async def get_media_info(self, media_type, media_id):
+        params = {
+            "language": "en",
+        }
+        async with Kiruha.http.get(
+                f'{OVERSEER_URL}/api/v1/{media_type}/{media_id}', self.headers, params=params
+        ) as response:
+            api_response = await response.json()
+            media_info = []
             if media_type == 'tv':
-                search_result = {
-                    "id":    result.get('id'),
-                    "title": result.get('name'),
-                    "year":  result.get('firstAirDate', '')[:4],
+                media_info = {
+                    "id":         api_response.get('id'),
+                    "title":      api_response.get('name'),
+                    "year":       api_response.get('firstAirDate', '')[:4],
+                    "media_type": media_type,
+                    "posterPath": api_response.get('posterPath'),
+                    "genres":     [genre['name'] for genre in api_response.get('genres', [])],
+                    "overview":   api_response.get('overview'),
                 }
             else:
-                search_result = {
-                    "id":    result.get('id'),
-                    "title": result.get('title'),
-                    "year":  result.get('releaseDate', '')[:4],
-
+                media_info = {
+                    "id":         api_response.get('id'),
+                    "title":      api_response.get('title'),
+                    "year":       api_response.get('releaseDate', '')[:4],
+                    "media_type": media_type,
+                    "posterPath": api_response.get('posterPath'),
+                    "genres":     [genre['name'] for genre in api_response.get('genres', [])],
+                    "overview":   api_response.get('overview'),
                 }
-            search_results.append(search_result)
-    print(search_results)
-    return search_results
 
+            return media_info
 
-async def get_media_info(media_type, media_id):
-    params = {
-        "language": "en",
-    }
-    async with Kiruha.http.get(f'{OVERSEER_URL}/api/v1/movie/{media_id}', headers, params=params) as response:
-        print(response)
-        api_response = await response.json()
-        return api_response
+    async def request_selected_media(self, media_type, media_id):
+        data = {
+            "mediaType":         media_type,
+            "mediaId":           int(media_id),
+            "tvdbId":            int(media_id),
+            "seasons":           "all",
+            "is4k":              False,
+            "serverId":          0,
+            "profileId":         0,
+            "rootFolder":        "string",
+            "languageProfileId": 0,
+            "userId":            0
+        }
+        async with Kiruha.http.post(
+                f'{OVERSEER_URL}/api/v1/request', self.headers, data=dumps(data)
+        ) as response:
+            api_response = await response.json()
+            print(api_response)
+        return "yay"
--- haruki/plugins/plex_request/plex_request.py
+++ haruki/plugins/plex_request/plex_request.py
@@ -3,28 +3,35 @@ __all__ = ()
 from hata import Embed
 from hata.ext.slash import Select, Option, InteractionResponse, Button, ButtonStyle, Row
 from haruki.bots import Kiruha
-from haruki.plugins.plex_request.overseer_search import get_search_results, get_media_info
+from haruki.plugins.plex_request.overseer_search import MediaSearch
 
 tmdb_image_url = 'https://image.tmdb.org/t/p/w300_and_h450_face'
 
 PLEX_REQUEST_ID = 'plex_request'
-PLEX_REQUEST_MEDIA = 'plex_request_media'
 PLEX_REQUEST_ABORT = 'plex_request_abort'
+PLEX_REQUEST_REQUEST = 'plex_request_request'
 
 MEDIA_TYPES = [
     'tv',
     'movie',
 ]
 
-BUTTON_REQUEST = Button('Request', custom_id=PLEX_REQUEST_MEDIA, style=ButtonStyle.green)
+BUTTON_REQUEST = Button('Request', custom_id=PLEX_REQUEST_REQUEST, style=ButtonStyle.green)
 BUTTON_ABORT = Button('Abort', custom_id=PLEX_REQUEST_ABORT, style=ButtonStyle.red)
 
+instances = {}
 
-@Kiruha.interactions(is_global=True)
-async def plex_request(media_type: (MEDIA_TYPES, 'Pick Media Type'), media: ('str', 'Enter the media to search for')):
-    media_list = await get_search_results(media_type, media)
+
+@Kiruha.interactions(is_global=True, name='plex_request')
+async def initiate_plex_request(
+        event, media_type: (MEDIA_TYPES, 'Pick Media Type'), media: ('str', 'Enter the media to search for')
+):
+    media_search = MediaSearch()
+    instances[event.user_id] = media_search
+    media_list = await media_search.get_search_results(media_type, media)
     embed = Embed(f'Searching for {media_type.title()}: {media.title()}')
     options = []
+    print(media_list)
     for element in media_list:
         name = element['title']
         year = element['year']
@@ -40,13 +47,13 @@ async def plex_request(media_type: (MEDIA_TYPES, 'Pick Media Type'), media: ('st
                 custom_id=PLEX_REQUEST_ID, )
         ),
         Row(
-            Button('Request', custom_id=PLEX_REQUEST_MEDIA, style=ButtonStyle.green),
-            Button('Abort', custom_id=PLEX_REQUEST_ABORT, style=ButtonStyle.red))]
-
+            Button('Request', custom_id=PLEX_REQUEST_REQUEST, style=ButtonStyle.green),
+            Button('Abort', custom_id=PLEX_REQUEST_ABORT, style=ButtonStyle.red)
+        )]
     return InteractionResponse(embed=embed, components=select)
 
 
-@Kiruha.interactions(custom_id=[PLEX_REQUEST_ID, PLEX_REQUEST_MEDIA, PLEX_REQUEST_ABORT])
+@Kiruha.interactions(custom_id=[PLEX_REQUEST_REQUEST, PLEX_REQUEST_ID])
 async def handle_media_selection(event):
     # We filter out 3rd party users based on original and current invoking user.
     if event.message.interaction.user_id != event.user_id:
@@ -55,21 +62,63 @@ async def handle_media_selection(event):
     # Second we filter out incorrect selected values.
     if selected_media_list is None:
         return
+    media_search = instances.get(event.user_id)
     selected_media = selected_media_list[0]
-    print(event.interaction.custom_id)
-    if event.interaction.custom_id == "plex_request_abort":
-        print("RIP")
-    elif event.interaction.custom_id == "plex_request_media":
-        print("Yay")
 
-    movie_info = await get_media_info("movie", selected_media)
-    url = tmdb_image_url + movie_info['posterPath']
-    embed = Embed(f'{movie_info["title"]} - {movie_info["releaseDate"].split("-")[0]}')
+    media_list = media_search.get_media_list()
+    options = []
+    for element in media_list:
+        name = element['title']
+        year = element['year']
+        id = element['id']
+        media_type = element['media_type']
+        if name:
+            title = f"{name} ({year})"
+            if str(id) == str(selected_media):
+                options.append(Option(str(id), title, default=True))
+                selected_id = id
+
+            else:
+                options.append(Option(str(id), title))
+    select = [
+        Row(
+            Select(
+                options,
+                custom_id=PLEX_REQUEST_ID, )
+        ),
+        Row(
+            Button('Request', custom_id=PLEX_REQUEST_REQUEST, style=ButtonStyle.green),
+            Button('Abort', custom_id=PLEX_REQUEST_ABORT, style=ButtonStyle.red)
+        )]
+    media_info = await media_search.get_media_info(media_type, selected_media)
+    url = tmdb_image_url + media_info['posterPath']
+    embed = Embed(f'{media_info["title"]} - {media_info["year"].split("-")[0]}')
     embed.add_image(url)
+    embed.add_author(f'{media_type} {selected_id}')
     embed.description = (
-        f'{movie_info["overview"]} \n'
+        f'{media_info["overview"]} \n')
 
-    )
-    yield InteractionResponse(embed=embed)
+    yield InteractionResponse(embed=embed, components=select)
+
+@Kiruha.interactions(custom_id=[PLEX_REQUEST_REQUEST])
+async def send_plex_request(client, event):
+    # Allow closing for the source user
+    if event.user is not event.message.interaction.user:
+        return
+    media_search = instances.get(event.user_id)
+    media_type, id = event.message.embed.author.name.split()
+    print(f'{media_type}: {id}')
+    await media_search.request_selected_media(media_type, id)
+    return
+
+@Kiruha.interactions(custom_id=[PLEX_REQUEST_ABORT])
+async def abort_plex_request(client, event):
+    # Allow closing for the source user
+    if event.user is not event.message.interaction.user:
+        return
+
+    # We can use `yield` as well for acknowledging it.
+    await client.interaction_component_acknowledge(event)
+    await client.interaction_response_message_delete(event)
 
 
