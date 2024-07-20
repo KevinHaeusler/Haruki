525686c5ac2a3ba6334284bc10d90c45c319261e
KevinHaeusler <kevin@kh.dev>
2024-07-20 22:35:54 +0200

    Refactor Plex activity embed creation

--- haruki/plugins/plex/get_activity.py
+++ haruki/plugins/plex/get_activity.py
@@ -15,38 +15,14 @@ async def plex_activity(client):
 async def build_activity_embed(client):
     activity_infos = await get_activity_info(client)
     embeds = []
+    TRIPLE_GRAVE = '`' * 3
 
     for activity_info in activity_infos:
-        if 'Artist' in activity_info:
-            embed = Embed(
-                title="Active Plex Session",
-                fields=[
-                    EmbedField(
-                        'Activity Details for: ',
-                        f'{block("User", activity_info["User"])}'
-                        f'{block("Artist", activity_info["Artist"])}'
-                        f'{block("Album", activity_info["Album"])}'
-                        f'{block("Song", activity_info["Song"])}'
-                        f'{block("Quality", activity_info["Quality"])}',
-                        inline=True,
-                    )
-                ]
-            )
-        else:
-            embed = Embed(
-                title="Active Plex Session",
-                fields=[
-                    EmbedField(
-                        'Activity Details for: ',
-                        f'{block("User", activity_info["User"])}'
-                        f'{block("Show", activity_info["Show"])}'
-                        f'{block("Season", activity_info["Season"])}'
-                        f'{block("Episode", activity_info["Episode"])}'
-                        f'{block("Quality", activity_info["Quality"])}',
-                        inline=True,
-                    )
-                ]
-            )
+        embed = Embed("Active Plex Session")
+
+        for name, value, inline in activity_info.iter_embed_field_values():
+            embed.add_field(name, f'{TRIPLE_GRAVE}\n{value}\n{TRIPLE_GRAVE}', inline)
+
         embed.add_image('https://i.imgur.com/BABpmZ3.png')
         embeds.append(embed)
 
--- haruki/plugins/plex/tautulli_api.py
+++ haruki/plugins/plex/tautulli_api.py
@@ -1,6 +1,50 @@
 from ...constants import TAUTULLI_TOKEN, TAUTULLI_URL
 
 
+class ActivityInfo:
+    __slots__ = ('user', 'quality')
+
+    def __init__(self, user, quality):
+        self.user = user
+        self.quality = quality
+
+    def iter_embed_field_values(self):
+        yield 'User', self.user, True
+        yield 'Quality', self.quality, True
+
+
+class TVShowActivityInfo(ActivityInfo):
+    __slots__ = ('show', 'season', 'episode')
+
+    def __init__(self, user, quality, show, season, episode):
+        ActivityInfo.__init__(self, user, quality)
+        self.show = show
+        self.season = season
+        self.episode = episode
+
+    def iter_embed_field_values(self):
+        yield from ActivityInfo.iter_embed_field_values(self)
+        yield 'Show', self.show, False
+        yield 'Season', self.season, False
+        yield 'Episode', self.episode, False
+
+
+class MusicActivityInfo(ActivityInfo):
+    __slots__ = ('artist', 'album', 'song')
+
+    def __init__(self, user, quality, artist, album, song):
+        ActivityInfo.__init__(self, user, quality)
+        self.artist = artist
+        self.album = album
+        self.song = song
+
+    def iter_embed_field_values(self):
+        yield from ActivityInfo.iter_embed_field_values(self)
+        yield 'Artist', self.artist, False
+        yield 'Album', self.album, False
+        yield 'Song', self.song, False
+
+
 async def get_activity_info(client):
     response = await make_api_call(client, 'get_activity')
 
@@ -17,32 +61,19 @@ async def get_activity_info(client):
     for session in sessions:
         if isinstance(session, dict):
             user = session.get('user', 'Unknown User')
-            grandparent_title = session.get('grandparent_title', '')
-            parent_title = session.get('parent_title', '')
-            title = session.get('title', 'Unknown Title')
             quality_profile = session.get('quality_profile', '')
             media_type = session.get('media_type', '')
 
+            grandparent_title = session.get('grandparent_title', '')
+            parent_title = session.get('parent_title', '')
+            title = session.get('title', 'Unknown Title')
+
             if media_type == 'track':
-                session_info = {
-                    "User":    user,
-                    "Artist":  grandparent_title,
-                    "Album":   parent_title,
-                    "Song":    title,
-                    "Quality": quality_profile
-                }
+                session_info = MusicActivityInfo(user, quality_profile, grandparent_title, parent_title, title)
             else:
-                session_info = {
-                    "User":    user,
-                    "Show":    grandparent_title,
-                    "Season":  parent_title,
-                    "Episode": title,
-                    "Quality": quality_profile
-                }
+                session_info = TVShowActivityInfo(user, quality_profile, grandparent_title, parent_title, title)
 
             activity_info.append(session_info)
-        else:
-            print(f"Unexpected session data type: {type(session)}, value: {session}")
     return activity_info
 
 
