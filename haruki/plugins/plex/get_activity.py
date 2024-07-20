70f58280f6c1570cf65ee6c25171200750da23f8
KevinHaeusler <kevin@kh.dev>
2024-07-20 21:06:12 +0200

    Refactor Plex activity display to use Embeds

--- haruki/plugins/plex/get_activity.py
+++ haruki/plugins/plex/get_activity.py
@@ -1,22 +1,57 @@
 __all__ = ()
 
-from hata import ClientWrapper
+from hata import Embed, EmbedField
+
 from haruki.bots import Kiruha
-from .tautulli_api import get_session_info, make_api_call  # import the helper function
-
-ALL = ClientWrapper(Kiruha)
+from .tautulli_api import get_activity_info
 
 
-@ALL.interactions(is_global=True)
+@Kiruha.interactions(is_global=True)
 async def plex_activity(client):
-    data = await make_api_call(client, 'get_activity')
-    sessions = data.get('response', {}).get('data', {}).get('sessions', [])
-    if sessions is None:
-        return "Invalid request."
-    if not sessions:
-        return "No active sessions."
-    result = ["**Active Plex Sessions:**"]
-    for session in sessions:
-        result.append(get_session_info(session))
-        # TODO refactor to use embeds instead
-    return "\n".join(result)
+    embed = await build_activity_embed(client)
+    return embed
+
+
+async def build_activity_embed(client):
+    activity_infos = await get_activity_info(client)
+    embeds = []
+
+    for activity_info in activity_infos:
+        if 'Artist' in activity_info:
+            embed = Embed(
+                title="Active Plex Session",
+                fields=[
+                    EmbedField(
+                        'Activity Details for: ',
+                        f'{block("User", activity_info["User"])}'
+                        f'{block("Artist", activity_info["Artist"])}'
+                        f'{block("Album", activity_info["Album"])}'
+                        f'{block("Song", activity_info["Song"])}'
+                        f'{block("Quality", activity_info["Quality"])}',
+                        inline=True,
+                    )
+                ]
+            )
+        else:
+            embed = Embed(
+                title="Active Plex Session",
+                fields=[
+                    EmbedField(
+                        'Activity Details for: ',
+                        f'{block("User", activity_info["User"])}'
+                        f'{block("Show", activity_info["Show"])}'
+                        f'{block("Season", activity_info["Season"])}'
+                        f'{block("Episode", activity_info["Episode"])}'
+                        f'{block("Quality", activity_info["Quality"])}',
+                        inline=True,
+                    )
+                ]
+            )
+        embeds.append(embed)
+
+    return embeds
+
+
+def block(title, value):
+    t = '`' * 3
+    return f'**{title}**\n{t}\n{value}\n{t}'
--- haruki/plugins/plex/tautulli_api.py
+++ haruki/plugins/plex/tautulli_api.py
@@ -1,68 +1,52 @@
 from ...constants import TAUTULLI_TOKEN, TAUTULLI_URL
 
 
-def get_session_info(session):
-    """
-    Get session information.
+async def get_activity_info(client):
+    response = await make_api_call(client, 'get_activity')
 
-    This method takes a session object as input and returns a formatted string containing session information.
+    if response is None:
+        return "Invalid request."
 
-    Parameters:
-    - session (dict): A dictionary representing the session object.
+    response_data = response.get('response', {}).get('data', {})
+    sessions = response_data.get('sessions')
 
-    Returns:
-    - session_info (str): A formatted string containing session information.
+    if not sessions:
+        return "No active sessions."
 
-    Example usage:
-    ```
-    session = {
-        'user': 'John Doe',
-        'grandparent_title': 'Artist',
-        'parent_title': 'Album',
-        'title': 'Song',
-        'quality_profile': 'High',
-        'media_type': 'track'
-    }
-    info = get_session_info(session)
-    print(info)
-    ```
+    activity_info = []
+    for session in sessions:
+        if isinstance(session, dict):
+            user = session.get('user', 'Unknown User')
+            grandparent_title = session.get('grandparent_title', '')
+            parent_title = session.get('parent_title', '')
+            title = session.get('title', 'Unknown Title')
+            quality_profile = session.get('quality_profile', '')
+            media_type = session.get('media_type', '')
 
-    Output:
-    ```
-    **User:**  `John Doe    `  **Artist:**  `Artist`  **Album:**  `Album`  **Song:**  `Song`  **Quality:**  `High`
-    ```
-    """
-    user = session.get('user', 'Unknown User')
-    grandparent_title = session.get('grandparent_title', '')
-    parent_title = session.get('parent_title', '')
-    title = session.get('title', 'Unknown Title')
-    quality_profile = session.get('quality_profile', '')
-    media_type = session.get('media_type', '')
-    if media_type == 'track':
-        session_info = ("**User:**  `{:<15} `  **Artist:**  `{}`  **Album:**  `{}`  **Song:**  `{}`  **Quality:**  "
-                        "`{}`".format(user, grandparent_title, parent_title, title, quality_profile))
-    else:
-        session_info = ("**User:**  `{:<15} `  **Show:**  `{}`  **Season:**  `{}`  **Episode:**  `{}`  **Quality:**  "
-                        "`{}`").format(user, grandparent_title, parent_title, title, quality_profile)
-    return session_info
+            if media_type == 'track':
+                session_info = {
+                    "User":    user,
+                    "Artist":  grandparent_title,
+                    "Album":   parent_title,
+                    "Song":    title,
+                    "Quality": quality_profile
+                }
+            else:
+                session_info = {
+                    "User":    user,
+                    "Show":    grandparent_title,
+                    "Season":  parent_title,
+                    "Episode": title,
+                    "Quality": quality_profile
+                }
+
+            activity_info.append(session_info)
+        else:
+            print(f"Unexpected session data type: {type(session)}, value: {session}")
+    return activity_info
 
 
 async def make_api_call(client, api_call):
-    """
-
-    Parameters:
-    - client: The http client used to make the api call.
-    - api_call: The specific api call to be made.
-
-    Returns:
-    The data obtained from the api call.
-
-    Example usage:
-    client = aiohttp.ClientSession()
-    api_call = "get_users"
-    result = await make_api_call(client, api_call)
-
-    """
     url = f"{TAUTULLI_URL}/api/v2?apikey={TAUTULLI_TOKEN}&cmd={api_call}"
     async with client.http.get(url) as response:
         data = await response.json()
