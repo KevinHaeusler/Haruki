36e0b41668207fe5ef40bf9cff38e9d360515d3e
KevinHaeusler <kevin@kh.dev>
2024-07-21 07:25:01 +0200

    Add methods to process parameters for different media types

--- haruki/plugins/plex/tautulli_api.py
+++ haruki/plugins/plex/tautulli_api.py
@@ -70,7 +70,24 @@ class MusicActivityInfo(ActivityInfo):
     def from_data(cls, user, quality_profile, grandparent_title, parent_title, title):
         return cls(user, quality_profile, grandparent_title, parent_title, title)
 
+def process_track_params(data):
+    return data[0], data[1], data[2], data[3], data[4]
 
+
+# Function to prepare parameters for TVShowActivityInfo
+def process_episode_params(data):
+    return data[0], data[1], data[2], data[3], data[4]
+
+
+# Function to prepare parameters for TVMovieActivityInfo
+def process_movie_params(data):
+    return data[0], data[1], data[4]
+
+media_type_to_class = {
+    'track':   (MusicActivityInfo.from_data, process_track_params),
+    'episode': (TVShowActivityInfo.from_data, process_episode_params),
+    'movie':   (TVMovieActivityInfo.from_data, process_movie_params),
+}
 async def get_activity_info(client):
     response = await make_api_call(client, 'get_activity')
 
@@ -92,17 +109,15 @@ async def get_activity_info(client):
         title = session.get('title', 'Unknown Title')
         media_type = session.get('media_type', '')
 
-        media_type_to_class = {
-            'track':   MusicActivityInfo,
-            'episode': TVShowActivityInfo,
-            'movie':   TVMovieActivityInfo,
-        }
+        # Collect all data for the from_data method into a list.
+        data = [user, quality_profile, grandparent_title, parent_title, title]
 
         if media_type not in media_type_to_class:
             return abort(f'Unknown media type: {media_type}')
 
-        ActivityClass = media_type_to_class[media_type]
-        session_info = ActivityClass.from_data(user, quality_profile, grandparent_title, parent_title, title)
+        ActivityClass, params_processor = media_type_to_class[media_type]
+        session_info = ActivityClass(*params_processor(data))
+
         activity_info.append(session_info)
 
     return activity_info
